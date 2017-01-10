936ce6d4656bac43947a6dac8a604dffc503855b
Arashiryuu <walkerdd01@googlemail.com>
2017-01-10 05:20:35 +0000

    Create greenText.plugin.js
    greentext plugin for discord

---
+++ greenText.plugin.js
@@ -0,0 +1,42 @@
+//META{"name":"gGreenText"}*//
+
+var greenText = function(){};
+var greenTextInterval;
+
+var greenTextMainFunction = function(){
+	$(".message-text>.markup").each(function() {
+		if(!$(this).find('.greenText').length) {
+			var strBase = $(this).text();
+			if(strBase.substr(0,1) == ">") {
+				$(this).text(strBase.substr(1, strBase.length-1));
+				$(this).addClass("greenText");
+			}
+		}
+	});
+};
+
+greenText.prototype.getName = function(){ return "greenText Plugin"; };
+greenText.prototype.getDescription = function(){ return "Use a \">\" at the start of a post to turn text green, like on 4chan."; };
+greenText.prototype.getVersion = function(){ return "1.0b"; };
+greenText.prototype.getAuthor = function(){ return "Arash"; };
+
+greenText.prototype.load = function(){
+	BdApi.injectCSS("greenText-InjectedStyleSheet", ".markup.greenText{color:#709900!important;}\
+.markup.greenText::before{content:'>'!important;}.markup.greenText:hover{font-weight:bold;}.mark\
+up.greenText:hover::before{content:'>'!important;font-weight: bold;}");
+};
+
+greenText.prototype.start = function(){
+	greenTextInterval = setInterval(greenTextMainFunction, 100);	//change this lower if needed
+};
+
+greenText.prototype.stop = function(){
+	clearInterval(greenTextInterval);
+};
+
+greenText.prototype.unload = function() {};
+
+greenText.prototype.onSwitch = function() {};
+greenText.prototype.onMessage = function(){};
+greenText.prototype.observer = function() {}
+greenText.prototype.getSettingsPanel = function(){ return "There are no settings here."; };
