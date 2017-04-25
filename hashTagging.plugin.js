674178ec7a700fa5293171e7d38183c464ad85e6
Arashiryuu <walkerdd01@googlemail.com>
2017-04-25 10:14:44 +0100

    Update hashTagging.plugin.js

--- hashTagging.plugin.js
+++ hashTagging.plugin.js
@@ -7,7 +7,7 @@ function processChat() {
 		$(".comment .body .markup:not(.line-scanned), .comment .markup>span:not(.line-scanned)").each(function() {
 			var tagRegex = /#[A-Z0-9a-z_-]+/igm;
 			var html = $(this).html();
-			$(this).html(html.replace(tagRegex, `<span style='color: #3898FF;'>$&</span>`));
+			$(this).html(html.replace(tagRegex, `<span style='color: #3898FF; font-weight: bold;'>$&</span>`));
 		}).addClass("line-scanned");
 	},250);
 };
