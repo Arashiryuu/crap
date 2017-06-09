29909edf4a766df039bcc9753f57e947045ed61f
Arashiryuu <walkerdd01@googlemail.com>
2017-06-10 00:24:24 +0100

    Update
    Progress?

--- hideUsers.plugin.js
+++ hideUsers.plugin.js
@@ -3,26 +3,54 @@
 class hideUsers {
 	constructor() {
 		this.hideUser = () => {
-			let users = [];
-			// edit user array to suit your needs, only accepts user IDs such as '140188899585687552' for example
-			if(!users[0]) return console.warn('%c[hideUsers]%c\tNo users found.', 'color: #9653AD', '');
-			for(let user of users) {
-				return $(`[style*='${user}']`).parent().hide();
+			if(!this.hidUsers.users[0]) return console.warn('%c[hideUsers]%c\tNo users found.', 'color: #9653AD', '');
+			for(let user of this.hidUsers.users) {
+  			$(`[style*='${user}']`).parent().hide();
  			}
-		}
+		};
+
+		this.hidUsers = {
+			users: [] /* example: '723504703284987254', '384793740543700787' */
+			// add users to array to hide them, only accepts user IDs which look like '252456997457231873'
+		};
 	};
 
-	start() { console.log('%c[hideUsers]%c\tWorking...', 'color: #9653AD', ''); return this.hideUser(); };
+	userPush(save) {
+		var nUser = $('#hUTEXT').val();
+		if(nUser === undefined || nUser === null) return $('#hUTEXT').val('Invalid entry.').css({"color":"red", "opacity":"1"});
+		this.hidUsers.users.push(nUser);
+		console.log(`%c[${this.getName()}]%c\t${this.hidUsers.users.join(', ')}`, 'color: #9653AD', '');
+		this.start();
+	};
+	
+	userClear() {
+		this.hidUsers.users.pop();
+		console.log(`%c[${this.getName()}]%c\t${this.hidUsers.users.join(', ')}`, 'color: #9653AD', '');
+		alert('Successfully removed!');
+		this.start();		
+	};
+
+	start() { this.hideUser(); console.log('%c[hideUsers]%c\tWorking...', 'color: #9653AD', ''); };
 	stop() { console.log('%c[hideUsers]%c\tStopped.', 'color: #9653AD', ''); };
-	load() { console.log('%c[hideUsers]%c\tBooting up.', 'color: #9653AD', ''); };
+	load() { console.log('%c[hideUsers]%c\tBooting-Up.', 'color: #9653AD', ''); };
 	unload() {};
 	onMessage() { this.hideUser(); };
 	onSwitch() { this.hideUser(); };
 
 	getAuthor() { return 'Arashiryuu'; };
 	getName() { return 'hideUsers'; };
-	getVersion() { return '0.1.0'; };
+	getVersion() { return '0.1.5'; };
 	getDescription() { return 'Hides any users listed in the array of words.'; };
-	getSettingsPanel() { return 'soon™'; };
+	getSettingsPanel() { 
+		let html = '<h3>soon™</h3><br/>'; 
+		html += '<input id="hUTEXT" type="text" placeholder="ID" style="resize: none; width: 80%;" /><br/><br/>';
+		html += '<br/><button class="hUBTNw" onclick=BdApi.getPlugin("'+ this.getName() +'").userPush()>apply</button>';
+		html += '<button class="hUBTNx" onclick=BdApi.getPlugin("'+ this.getName() +'").userClear()>remove</button><br/>';
+		html += '<br/>How to use:';
+		html += '<br/>1) Insert a user\'s ID.<br/>';
+		html += '2) Click "apply."<br/>';
+		html += '3) To remove the last-added user, click the "remove" button.<br/>';
+		return html;
+	};
 };
 /*@end*/
