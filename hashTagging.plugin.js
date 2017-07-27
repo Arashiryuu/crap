40694ddf2381d895065ae7c155ae14032d14e2b2
Arashiryuu <walkerdd01@googlemail.com>
2017-07-28 00:26:27 +0100

    Update hashTagging.plugin.js

--- hashTagging.plugin.js
+++ hashTagging.plugin.js
@@ -7,7 +7,7 @@ class hashTagging {
 		$(".comment .body .markup:not(.line-scanned), .comment .markup>span:not(.line-scanned)").each(function() {
 			var tagRegex = /\B#[A-Z0-9a-z_-]+/igm;
 			var html = $(this).html();
-			$(this).html(html.replace(tagRegex, `<span style='color: #3898FF; font-weight: bold;'>$&</span>`));
+			$(this).html(html.replace(tagRegex, `<span id="hashtag" style='color: #3898FF; font-weight: bold;'>$&</span>`));
 		}).addClass("line-scanned");
 	},250);
    }
@@ -36,9 +36,9 @@ class hashTagging {
   onSwitch() { setTimeout(() => this.processChat(), 250); }
 
   getName() { return 'hashTagging'; }
-  getDescription() { return 'Start a word or sentence with a \"#\" to hashtag.'; }
   getAuthor() { return 'Arashiryuu'; }
   getVersion() { return '1.2.0'; }
+	getDescription() { return 'Start a word or sentence with a \"#\" to hashtag.'; }
   getSettingsPanel() { return 'Go away!'; }
 };
 /*@end @*/
