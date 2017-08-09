a4b250df5902eb338eda0db52a7c8e0a0f27af36
Arashiryuu <walkerdd01@googlemail.com>
2017-08-09 08:02:34 +0100

    Update hideUsers.plugin.js

--- hideUsers.plugin.js
+++ hideUsers.plugin.js
@@ -1,5 +1,28 @@
 //META{"name":"hideUsers"}*//
 
+/*@cc_on
+@if (@_jscript)
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+@else@*/
+
 class hideUsers {
 	constructor() {
 		this.hideUser = () => {
@@ -32,13 +55,20 @@ class hideUsers {
 	};
 	
 	userClear() {
-		if(this.hidUsers.users.length !== 0) {
+		let oUser = $('#hUTEXT').val();
+		if(oUser.match(/^\d{17,18}$/)) {
+			this.hidUsers.users.splice(this.hidUsers.users.indexOf(oUser), 1);
+			console.info(`%c[${this.getName()}]%c\t${this.hidUsers.users.join(', ')}`, 'color: #9653AD', '');
+			alert('Successfully removed!');
+			this.hideUser();
+		}
+		else {
 			this.hidUsers.users.pop();
 			console.info(`%c[${this.getName()}]%c\t${this.hidUsers.users.join(', ')}`, 'color: #9653AD', '');
 			alert('Successfully removed!');
 			this.hideUser();
 		}
-		else
+		if(this.hidUsers.users.length < 1)
 			return console.info(`%c[${this.getName()}]%c\t No users to remove.`, 'color: #9653AD', '');
 	};
 
