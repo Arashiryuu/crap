0162c763986d106296482b6032c0b38e0afeaf76
Arashiryuu <walkerdd01@googlemail.com>
2017-08-09 07:58:43 +0100

    Update hideChannels.plugin.js

--- hideChannels.plugin.js
+++ hideChannels.plugin.js
@@ -1,11 +1,34 @@
 //META{"name":"hideChannels"}*//
 
+/*@cc_on
+@if (@_jscript)
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+@else@*/
+
 class hideChannels {
 	constructor() {
 		this.hideChannel = () => {
 			if(!this.hidChannels.chans[0]) return console.warn('%c[hideChannels]%c\tNo channels found.', 'color: #F2F', '');
 			for(let chan of this.hidChannels.chans) {
-  			$(`.containerDefault-7RImuF:contains(${chan})`).hide();
+  				$(`.containerDefault-7RImuF:contains(${chan})`).hide();
  			}
 		};
 
@@ -17,13 +40,20 @@ class hideChannels {
 	chanPush() {
 		let nChan = $('#ChanblockField').val();
 		if(typeof nChan !== 'string') return $('#ChanblockField').val('Invalid entry. (Name-only)');
-		if(nChan.length === 0 || nChan === undefined) return $('#ChanblockField').val('Invalid entry. (No-entry)');
+		if(!nChan) return $('#ChanblockField').val('Invalid entry. (No-entry)');
 		this.hidChannels.chans.push(nChan);
 		console.info(`%c[${this.getName()}]%c\t${this.hidChannels.chans.join(', ')}`, 'color: #F2F', '');
 		this.hideChannel();
 	};
 	
 	chanClear() {
+		let oCh = $('#ChanblockField').val();
+		if(oCh.length) {
+			this.hidChannels.chans.splice(this.hidChannels.chans.indexOf(oCh), 1);
+			console.info(`%c[${this.getName()}]%c\t${this.hidChannels.chans.join(', ')}`, 'color: #F2F', '');
+			alert('Successfully removed!');
+			this.hideChannel();	
+		}
 		this.hidChannels.chans.pop();
 		console.info(`%c[${this.getName()}]%c\t${this.hidChannels.chans.join(', ')}`, 'color: #F2F', '');
 		alert('Successfully removed!');
