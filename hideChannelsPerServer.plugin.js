82ddfe33d1a5e88b54176055daf8d2c2affa9e7b
Arashiryuu <walkerdd01@googlemail.com>
2017-10-01 16:01:17 +0100

    Update hideChannelsPerServer.plugin.js

--- hideChannelsPerServer.plugin.js
+++ hideChannelsPerServer.plugin.js
@@ -1,40 +1,10 @@
 //META{"name":"hideChannelsPerServer"}*//
 
-/*@cc_on
-@if (@_jscript)
-	
-	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-	var pathSelf = WScript.ScriptFullName;
-	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-		// Show the user where to put plugins in the future
-		shell.Exec("explorer " + pathPlugins);
-		shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
-	}
-	WScript.Quit();
-@else@*/
-
-/**
- * @name hideChannelsPerServer
- * @author Arashiryuu
- * @description Plugin for BetterDiscord -- allows per-server channel hiding via snowflakes. (IDs)
- * @version ƒ1
- */
-
 class hideChannelsPerServer {
 	constructor() {
 		this.hideChannel = () => {
 			if(!this.hidChannels.chans[0]) {
-				$('.channels-wrap [class*="containerDefault-"]').each(function() {
+				$('.channels-wrap [class*="containerDefault-7RImuF"]').each(function() {
 					if($(this).css('display') === 'none') $(this).show();
 				});
 				return console.warn('%c[hideChannelsPerServer]%c\tNo channels found.', 'color: #F2F', '');
@@ -42,26 +12,23 @@ class hideChannelsPerServer {
 			const self = this;
 			if(window.DiscordInternals !== null) {
 				const { getOwnerInstance } = window.DiscordInternals;
-				$('.channels-wrap [class*="containerDefault-"]').each(function() {
-  					self.hidChannels.chans.some(i => i === getOwnerInstance($(this)[0], {}).props.channel.id) ? $(this).hide() : $(this).show();
+				$('.channels-wrap [class*="containerDefault-7RImuF"]').each(function() {
+  				self.hidChannels.chans.some(i => i === getOwnerInstance($(this)[0], {}).props.channel.id) ? $(this).hide() : $(this).show();
 				});
 			}
 			else {
-				$('.channels-wrap [class*="containerDefault-"]').each(function() {
+				$('.channels-wrap [class*="containerDefault-7RImuF"]').each(function() {
 					self.hidChannels.chans.some(ii => ii === self.getReactInstance($(this)[0])._currentElement.props.children.props.channel.id) ? $(this).hide() : $(this).show();
 				});
 			}
 		};
-
 		this.hidChannels = {
 			chans: []
 		};
 	};
-	
 	getReactInstance(node) {
 		return node[Object.keys(node).find((key) => key.startsWith('__reactInternalInstance'))];
-	}
-
+	};
 	chanPush() {
 		let nChan = $('#ChanblockField').val();
 		if(isNaN(nChan)) return $('#ChanblockField').val('Invalid entry. (ID-only)');
@@ -71,7 +38,6 @@ class hideChannelsPerServer {
 		console.info(`%c[${this.getName()}]%c\t${this.hidChannels.chans.join(', ')}`, 'color: #F2F', '');
 		this.hideChannel();
 	};
-	
 	chanClear() {
 	  let oChan = $('#ChanblockField').val();
 		if(oChan.match(/^\d{16,18}$/)) {
@@ -79,25 +45,23 @@ class hideChannelsPerServer {
 			console.info(`%c[${this.getName()}]%c\t${this.hidChannels.chans.join(', ')}`, 'color: #F2F', '');
 			alert('Successfully removed!');
 			this.hideChannel();
+		} else {
+			this.hidChannels.chans.pop();
+			console.info(`%c[${this.getName()}]%c\t${this.hidChannels.chans.join(', ')}`, 'color: #F2F', '');
+			alert('Successfully removed!');
+			this.hideChannel();	
 		}
-		this.hidChannels.chans.pop();
-		console.info(`%c[${this.getName()}]%c\t${this.hidChannels.chans.join(', ')}`, 'color: #F2F', '');
-		alert('Successfully removed!');
-		this.hideChannel();	
 	};
-
 	saveSettings() {
 		bdPluginStorage.set('hideChannelsPerServer', 'channelsss', JSON.stringify(this.hidChannels.chans));
 		console.info('%c[hideChannelsPerServer]%c\tSaved settings.', 'color: #F2F', '');
 		console.info('%c[hideChannelsPerServer]%c\t' + this.hidChannels.chans.join(', '), 'color: #F2F', '');
 	};
-
 	loadSettings() {
 		this.hidChannels.chans = JSON.parse(bdPluginStorage.get('hideChannelsPerServer', 'channelsss'));
 		console.info('%c[hideChannelsPerServer]%c\tLoaded settings.', 'color: #F2F', '');
 		console.info('%c[hideChannelsPerServer]%c\t' + this.hidChannels.chans.join(', '), 'color: #F2F', '');
 	};
-
 	start() {
 		console.info('%c[hideChannelsPerServer]%c\tWorking...', 'color: #F2F', '');
 		var settings = bdPluginStorage.get('hideChannelsPerServer', 'channelsss');
@@ -110,28 +74,43 @@ class hideChannelsPerServer {
 		}
 		this.hideChannel();
 	};
-	stop() { console.info('%c[hideChannelsPerServer]%c\tStopped.', 'color: #F2F', ''); };
-	load() { console.info('%c[hideChannelsPerServer]%c\tBooting-Up.', 'color: #F2F', ''); };
-	onSwitch() { this.hideChannel(); };
-
-	getAuthor() { return 'Arashiryuu'; };
-	getName() { return 'hideChannelsPerServer'; };
-	getVersion() { return 'ƒ1'; };
-	getDescription() { return 'Hides any channels listed in the array of IDs.'; };
+	stop() {
+		$('.channels-wrap [class*="containerDefault-7RImuF"]').each(function() {
+			if($(this).css('display') === 'none') $(this).show();
+		});
+		console.info('%c[hideChannelsPerServer]%c\tStopped.', 'color: #F2F', ''); 
+	};
+	load() { 
+		console.info('%c[hideChannelsPerServer]%c\tBooting-Up.', 'color: #F2F', ''); 
+	};
+	onSwitch() { 
+		this.hideChannel(); 
+	};
+	getAuthor() { 
+		return 'Arashiryuu'; 
+	};
+	getName() { 
+		return 'hideChannelsPerServer'; 
+	};
+	getVersion() { 
+		return '1.1'; 
+	};
+	getDescription() {
+		return 'Hides any channels listed in the array of IDs.'; 
+	};
 	getSettingsPanel() { 
-		let htmlz = `
-			<h3>hideChannelsPerServer Plugin</h3><br/> 
-			<input id="ChanblockField" type="text" placeholder="ID" style="resize: none; width: 80%;" /><br/><br/>
-	 		<br/><button class="ChU-btn0" onclick=BdApi.getPlugin('${this.getName()}').chanPush()>apply</button>
-			<button class="ChU-btn1" onclick=BdApi.getPlugin('${this.getName()}').chanClear()>remove</button>
-	 		<button class="ChU-btn2" onclick=BdApi.getPlugin('${this.getName()}').saveSettings()>save</button>
-			<button class="ChU-btn3" onclick=BdApi.getPlugin('${this.getName()}').loadSettings()>load</button><br/>
-			<br/>How to use:<br/>
-			1) Insert a channel\'s ID.<br/>
-	 		2) Click "apply."<br/>
-			3) To remove the last-added channel, click the "remove" button.<br/>
-		`.trim();
-		return htmlz;
+		let htmls = '<h3>hideChannelsPerServer Plugin</h3><br/>'; 
+		htmls += '<input id="ChanblockField" type="text" placeholder="ID" style="resize: none; width: 80%;" /><br/><br/>';
+		htmls += '<br/><button class="ChU-btn0" onclick=BdApi.getPlugin("'+ this.getName() +'").chanPush()>apply</button>';
+		htmls += '<button class="ChU-btn1" onclick=BdApi.getPlugin("'+ this.getName() +'").chanClear()>remove</button>';
+		htmls += '<button class="ChU-btn2" onclick=BdApi.getPlugin("'+ this.getName() +'").saveSettings()>save</button>';
+		htmls += '<button class="ChU-btn3" onclick=BdApi.getPlugin("'+ this.getName() +'").loadSettings()>load</button><br/>';
+		htmls += '<br/>How to use:';
+		htmls += '<br/>1) Insert a channel\'s ID.<br/>';
+		htmls += '2) Click "apply."<br/>';
+		htmls += '3) To remove the last-added channel, click the "remove" button.<br/>';
+		return htmls;
 	};
 };
+
 /*@end*/
