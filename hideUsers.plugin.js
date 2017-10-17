eb40653959bd0ce6a2ec933c164cc1fd1c687c63
Arashiryuu <walkerdd01@googlemail.com>
2017-10-17 13:16:06 +0100

    Update hideUsers.plugin.js
    immutability where applicable; context-menu support, can now right-click a user and click "Hide User" to hide them.

--- hideUsers.plugin.js
+++ hideUsers.plugin.js
@@ -9,7 +9,7 @@
 	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
 	var pathSelf = WScript.ScriptFullName;
 	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
 	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
 		shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
 	} else if (!fs.FolderExists(pathPlugins)) {
@@ -21,116 +21,223 @@
 		shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
 	}
 	WScript.Quit();
+
 @else@*/
 
 class hideUsers {
 	constructor() {
-		this.hideUser = () => {
-			if(!this.hidUsers.users[0]) return console.warn('%c[hideUsers]%c\tNo users found.', 'color: #9653AD', '');
-			for(let user of this.hidUsers.users) {
-				$(`[style*='${user}']`).parent().hide();
-				$(`.avatarContainer-303pFz [style*='${user}']`).parent().parent().parent().hide();
- 			}
-		};
-
 		this.hidUsers = {
 			users: []
 		};
-		
+
 		this.blockCSS = `
 			<style id="hideUsersCSS">
-			.message-group-blocked { display: none; }
+				.message-group-blocked { display: none; }
 			</style>
 		`;
+
+		this.contextItem = `<div class="item-group hideUsers">
+			<div class="item hideUser-item">
+				<span>Hide User</span>
+				<div class="hint"></div>
+			</div>
+		</div>`;
+
+		this.contextmo = new MutationObserver((changes, p) => {
+			changes.forEach((change, i) => {
+				if(change.addedNodes) {
+					change.addedNodes.forEach((node) => {
+						if(node.nodeType === 1 && node.classList && node.classList.contains('context-menu')) {
+							this.appendContext(node);
+						}
+					});
+				}
+			});
+		});
 	};
 
+	hideUser() {
+		if(!this.hidUsers.users[0]) {
+			$('.message-group').each(function() {
+				if($(this).css('display') === 'none') $(this).show();
+			});
+			$('.member').each(function() {
+				if($(this).css('display') === 'none') $(this).show();
+			});	
+			return this.log('No users found');
+		}
+		for(const user of this.hidUsers.users) {
+			$(`[style*='${user}']`).parent().hide();
+			$(`.avatarContainer-303pFz [style*='${user}']`).parent().parent().parent().hide();
+		}
+	}
+
+	appendContext(context) {
+    if(!context) return;
+		if((this.getReactInstance(context).return.memoizedProps.target && this.getReactInstance(context).return.memoizedProps.target.classList.contains('avatar-large'))
+		|| (this.getReactInstance(context).return.memoizedProps.target && this.getReactInstance(context).return.memoizedProps.target.classList.contains('user-name'))
+		|| (this.getReactInstance(context).return.memoizedProps.target && this.getReactInstance(context).return.memoizedProps.target.classList.contains('member-username'))
+		|| (this.getReactInstance(context).return.memoizedProps.target && this.getReactInstance(context).return.memoizedProps.target.classList.contains('avatar-small'))) {
+      $(context).find('.item:contains("Profile")').after(this.contextItem);
+      $(context).find('.item.hideUser-item')
+        .off('click.hideUsers')
+        .on('click.hideUsers', this.contextHide.bind(this));
+    }
+	}
+	
+	contextHide() {
+    if(!$('.context-menu').length) return;
+    if(!this.getReactInstance($('.context-menu')[0]).return.stateNode.props.user) return;
+    if(!this.hidUsers.users.includes(this.getReactInstance($('.context-menu')[0]).return.stateNode.props.user.id)) {
+      this.hidUsers.users.push(this.getReactInstance($('.context-menu')[0]).return.stateNode.props.user.id);
+      this.saveSettings();
+      this.hideUser();
+		}
+  }
+
 	userPush() {
-		let nUser = $('#hUTEXT').val();
-		if(isNaN(nUser)) return $('#hUTEXT').val('Invalid entry. (NaN)');
-		if(nUser.length === 0 || nUser === undefined) return $('#hUTEXT').val('Invalid entry. (No-entry)');
-		if(!nUser.match(/^\d{17,18}$/) || nUser.match(/^\d{17,18}$/) === null) return $('#hUTEXT').val('Invalid entry. (ID-length)');
+		const nUser = $('#blockField').val();
+		if(isNaN(nUser)) return $('#blockField').val('Invalid entry. (NaN)');
+		if(nUser.length === 0 || nUser === undefined) return $('#blockField').val('Invalid entry. (No-entry)');
+		if(!nUser.match(/^\d{17,18}$/)) return $('#blockField').val('Invalid entry. (ID-length)');
 		this.hidUsers.users.push(nUser);
-		console.log(`%c[${this.getName()}]%c\t${this.hidUsers.users.join(', ')}`, 'color: #9653AD', '');
+		this.log(this.hidUsers.users.join(', '));
 		this.hideUser();
 	};
 	
 	userClear() {
-		let oUser = $('#hUTEXT').val();
-		if(oUser.match(/^\d{17,18}$/)) {
+	 const oUser = $('#blockField').val();
+	 if(this.hidUsers.users.length !== 0) {
+	 	if(oUser.match(/^\d{17,18}$/)) {
 			this.hidUsers.users.splice(this.hidUsers.users.indexOf(oUser), 1);
-			console.info(`%c[${this.getName()}]%c\t${this.hidUsers.users.join(', ')}`, 'color: #9653AD', '');
 			alert('Successfully removed!');
+			this.log(this.hidUsers.users.join(', '));
 			this.hideUser();
 		}
 		else {
 			this.hidUsers.users.pop();
-			console.info(`%c[${this.getName()}]%c\t${this.hidUsers.users.join(', ')}`, 'color: #9653AD', '');
 			alert('Successfully removed!');
+			this.log(this.hidUsers.users.join(', '));
 			this.hideUser();
 		}
-		if(this.hidUsers.users.length < 1)
-			return console.info(`%c[${this.getName()}]%c\t No users to remove.`, 'color: #9653AD', '');
+	 }
+	 else {
+		 this.log('No users available');
+	 }
 	};
 
 	saveSettings() {
 		bdPluginStorage.set('hideUsers', 'users', JSON.stringify(this.hidUsers.users));
-		console.log('%c[hideUsers]%c\tSaved settings.', 'color: #9653AD', '');
+		this.log('Saved settings\n' + this.hidUsers.users.join(', '));
 	};
 
 	loadSettings() {
-		this.hidUsers.users = JSON.parse(bdPluginStorage.get('hideUsers', 'users'));
-		console.log('%c[hideUsers]%c\tLoaded settings.', 'color: #9653AD', '');
+		const settings = bdPluginStorage.get('hideUsers', 'users');
+		if(settings) {
+			this.hidUsers.users = JSON.parse(settings);
+			this.log('Loaded settings\n' + this.hidUsers.users.join(', '));
+		}
+		else {
+			this.log('No settings found');
+		}
 	};
 
+	/**
+   * @name getInternalInstance
+   * @description returns the react internal instance of the element
+   * @param {Node} node - the element we want the internal data from
+   * @author noodlebox
+   * @returns {Node}
+   */
+	getReactInstance(node) {
+		return node[Object.keys(node).find((key) => key.startsWith('__reactInternalInstance'))];
+	}
+
 	start() { 
-		console.info('%c[hideUsers]%c\tWorking...', 'color: #9653AD', '');
-		var settings = bdPluginStorage.get('hideUsers', 'users');
-		if(settings === null) {
-			console.info('%c[hideUsers]%c\tNo settings found.', 'color: #9653AD', '');
+		this.log('Started');
+		const settings = bdPluginStorage.get('hideUsers', 'users');
+		if(!settings) {
+			this.log('No settings found');
 		}
 		else {
 			this.hidUsers.users = JSON.parse(settings);
-			console.info('%c[hideUsers]%c\t' + this.hidUsers.users.join(', '), 'color: #9653AD', '');
+			this.log('Loaded settings\n' + this.hidUsers.users.join(', '));
 		}
-	 	this.hideUser();
-		$('head').append(this.blockCSS);
+		 this.hideUser();
+		 this.contextmo.observe($('.app')[0], {childList: true, subtree: true});
+	 	$('head').append(this.blockCSS);
 	};
-	stop() { console.info('%c[hideUsers]%c\tStopped.', 'color: #9653AD', ''); };
-	load() { console.info('%c[hideUsers]%c\tBooting-Up.', 'color: #9653AD', ''); };
-	unload() {};
-	observer(ex) { 
-		if(ex.addedNodes.length && ex.addedNodes[0].classList && ex.addedNodes[0].classList.contains('message-group'))
-			this.hideUser();
-		if(ex.addedNodes.length && ex.addedNodes[0].classList && ex.addedNodes[0].classList.contains('markup'))
-			this.hideUser();
-		if(ex.addedNodes.length && ex.addedNodes[0].classList && ex.addedNodes[0].classList.contains('channel-members-wrap'))
-			this.hideUser();
-		else 
-			return; 
+
+	stop() {
+		this.contextmo.disconnect();
+		$('#hideUsersCSS').remove();
+		$('.message-group').each(function() {
+			if($(this).css('display') === 'none') $(this).show();
+		});
+		$('.member').each(function() {
+			if($(this).css('display') === 'none') $(this).show();
+		});
+		$('*').off('click.hideUsers');
+		this.log('Stopped');
 	};
-	onMessage() {};
-	onSwitch() { this.hideUser(); };
 
-	getAuthor() { return 'Arashiryuu'; };
-	getName() { return 'hideUsers'; };
-	getVersion() { return '1.2.3'; };
-	getDescription() { return 'Hides any users listed in the array.'; };
-	getSettingsPanel() { 
-		let html = `
-			<h3>hideUsers Plugin</h3><br/>
+	load() { 
+		this.log('Loaded');
+	};
 
-			<input id="hUTEXT" type="text" placeholder="ID" style="resize: none; width: 80%;" /><br/><br/>
-			<br/><button class="hUBTNw" onclick=BdApi.getPlugin("${this.getName()}").userPush()>apply</button>
-			<button class="hUBTNx" onclick=BdApi.getPlugin("${this.getName()}").userClear()>remove</button>
-			<button class="hUBTNy" onclick=BdApi.getPlugin("${this.getName()}").saveSettings()>save</button>
-			<button class="hUBTNz" onclick=BdApi.getPlugin("${this.getName()}").loadSettings()>load</button><br/>
-			
-			<br/>How to use:<br/>
-			0) Go to user settings -> Appearance, and enable Developer Mode, then right-click a user and "Copy ID"<br/>
-			1) Insert a user\'s ID.<br/>
-			2) Click "apply."<br/>
-			3) To remove the last-added user, click the "remove" button.<br/>`;
-		return html;
+	log(text) {
+		return console.log(`[%c${this.getName()}%c] ${text}`, 'color: #9653AD', '');
+	}
+
+	observer(ex) {
+		if(ex.addedNodes.length && ex.addedNodes[0].classList && ex.addedNodes[0].classList.contains('message-group')) {
+			this.hideUser();
+		}
+		if(ex.addedNodes.length && ex.addedNodes[0].classList && ex.addedNodes[0].classList.contains('markup')) {
+			this.hideUser();
+		}
+		if(ex.addedNodes.length && ex.addedNodes[0].classList && ex.addedNodes[0].classList.contains('channel-members-wrap')) {
+			this.hideUser();
+		}
+	};
+
+	onSwitch() { 
+		this.hideUser(); 
+	};
+
+	getName() {
+		return 'hideUsers';
+	};
+
+	getAuthor() {
+		return 'Arashiryuu';
+	};
+
+	getVersion() {
+		return '1.3';
+	};
+
+	getDescription() {
+		return 'Hides any users listed in the array of IDs.';
+	};
+
+	getSettingsPanel() {
+		return `<div id="huplugin-settings-div">
+		<h3>hideUsers Plugin</h3><br/><br/>
+
+		<input id="blockField" type="text" placeholder="ID" style="resize: none; width: 80%;" /><br/><br/>
+		<br/><button class="hU-btn0" onclick=BdApi.getPlugin("${this.getName()}").userPush()>apply</button>
+		<button class="hU-btn1" onclick=BdApi.getPlugin("${this.getName()}").userClear()>remove</button>
+		<button class="hU-btn2" onclick=BdApi.getPlugin("${this.getName()}").saveSettings()>save</button>
+		<button class="hU-btn3" onclick=BdApi.getPlugin("${this.getName()}").loadSettings()>load</button><br/>
+		
+		<br/>How to use:<br/>
+		0) User Settings \u21D2 Appearence \u21D2 Developer Mode, then right-click a user and "Copy ID".<br/>
+		1) Insert a user's ID.<br/>
+		2) Click "apply."<br/>
+		3) To remove the last-added user, click the "remove" button.<br/>
+		</div>`;
 	};
 };
+
 /*@end*/
