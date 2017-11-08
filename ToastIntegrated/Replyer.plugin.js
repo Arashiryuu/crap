45e8ad10b4030e2a8079b1fc99d94b8c0faa23cb
Arashiryuu <walkerdd01@googlemail.com>
2017-11-08 16:59:06 +0000

    1.0.1

--- ToastIntegrated/Replyer.plugin.js
+++ ToastIntegrated/Replyer.plugin.js
@@ -136,6 +136,55 @@ class Replyer {
 	err(error) {
 		return console.error(`[%c${this.getName()}%c] ${error}`, 'color: #59F;', '');
 	}
+	
+	checkForUpdate() {
+		const githubRaw = `https://raw.githubusercontent.com/Arashiryuu/crap/master/ToastIntegrated/${this.getName()}.plugin.js`;
+		$.get(githubRaw, (result) => {
+			let ver = result.match(/['"][0-9]+\.[0-9]+\.[0-9]+['"]/i);
+			if (!ver) return;
+			ver = ver.toString().replace(/"/g, "");
+			ver = ver.split(".");
+			const lver = this.getVersion().split(".");
+			let hasUpdate = false;
+			if (ver[0] > lver[0]) hasUpdate = true;
+			else if (ver[0] == lver[0] && ver[1] > lver[1]) hasUpdate = true;
+			else if (ver[0] == lver[0] && ver[1] == lver[1] && ver[2] > lver[2]) hasUpdate = true;
+			if (hasUpdate) this.showUpdateNotice(this.getName());
+			else this.removeUpdateNotice(this.getName());
+		});
+	}
+
+	showUpdateNotice() {
+		const updateLink = `https://betterdiscord.net/ghdl?url=https://github.com/Arashiryuu/crap/blob/master/ToastIntegrated/${this.getName()}.plugin.js`;
+		BdApi.clearCSS('pluginNoticeCSS');
+		BdApi.injectCSS('pluginNoticeCSS', '#pluginNotice span, #pluginNotice span a {-webkit-app-region: no-drag;color:#fff;} #pluginNotice span a:hover {text-decoration:underline;}');
+		const noticeElement = '<div class="notice notice-info" id="pluginNotice"><div class="notice-dismiss" id="pluginNoticeDismiss"></div>The following plugins have updates: &nbsp;<strong id="outdatedPlugins"></strong></div>';
+		if (!$('#pluginNotice').length)  {
+			$('.app.flex-vertical').children().first().before(noticeElement);
+			$('.win-buttons').addClass('win-buttons-notice');
+			$('#pluginNoticeDismiss').on('click', () => {
+				$('.win-buttons').animate({ top: 0 }, 400, 'swing', () => $('.win-buttons').css('top', '').removeClass('win-buttons-notice'));
+				$('#pluginNotice').slideUp({ complete: () => $('#pluginNotice').remove() });
+			});
+		}
+		const pluginNoticeID = `${this.getName()}-notice`;
+		if (!$(`#${pluginNoticeID}`).length) {
+			let pluginNoticeElement = $(`<span id="${pluginNoticeID}">`);
+			pluginNoticeElement.html(`<a href="${updateLink}" target="_blank">${this.getName()}</a>`);
+			if ($('#outdatedPlugins').children('span').length) $('#outdatedPlugins').append('<span class="separator">, </span>');
+			$('#outdatedPlugins').append(pluginNoticeElement);
+		}
+	}
+
+	removeUpdateNotice() {
+		const notice = $(`#${this.getName()}-notice`);
+		if (notice.length) {
+			if (notice.next('.separator').length) notice.next().remove();
+			else if (notice.prev('.separator').length) notice.prev().remove();
+			notice.remove();
+		}
+		if (!$('#outdatedPlugins').children('span').length) $('#pluginNoticeDismiss').click();
+	}
 
 	getName() {
 		return 'Replyer';
@@ -146,7 +195,7 @@ class Replyer {
 	}
 
 	getVersion() {
-		return '1.0.0';
+		return '1.0.1';
 	}
 
 	getDescription() {
