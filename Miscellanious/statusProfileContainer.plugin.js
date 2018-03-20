11d2c7c68afae351e48adc043c1f703ad8848f0b
Arashiryuu <walkerdd01@googlemail.com>
2018-03-20 05:43:04 +0000

    Update statusProfileContainer.plugin.js

--- Miscellanious/statusProfileContainer.plugin.js
+++ Miscellanious/statusProfileContainer.plugin.js
@@ -2,40 +2,55 @@
 
 class statusProfileContainer {
 	constructor() {
-		this.color = null;
-		this.statuses = null;
-		this.interval = null;
-		this.id = null;
+		this.o = 1;
+		this.id = '238108500109033472';
+		this.e = `.avatar-small[style*="${this.id}"] .status`;
+		this.statuses = {
+			'online': 'rgb(67, 181, 129)',
+			'idle': 'rgb(250, 166, 26)',
+			'dnd': 'rgb(240, 71, 71)',
+			'invisible': 'rgb(116, 127, 141)',
+			'streaming': 'rgb(89, 54, 149)'
+		};
 	}
 	start() {
 		this.log('Starting');
-		this.interval = setInterval(function() {
-			this.id = '238108500109033472'; // my user ID
-			this.color = $(`.avatar-small[style*="${this.id}"] .status`).css('border-color');
-			this.statuses = ['online', 'dnd', 'idle', 'invisible', 'streaming'];
-			const self = this;
-			self.color = this.color;
-			self.id = this.id;
-			this.statuses.some(
-				(stat) => $(`.avatar-small[style*="${self.id}"] .status`).attr('class').includes(stat)) ? (
-				$('.container-iksrDt').css({'background':self.color}) 
-				) : ( 
-				''
-				);
-		}, 1000);
+		this.o = 1;
+		const status = $(this.e).attr('class');
+		this.colorize();
+		this.run(status);
+	}
+	run(o) {
+		if(this.o) {
+			const status = $(this.e).attr('class');
+	
+			if(status === o) {
+				return setTimeout(() => this.run(o), 250);
+			}
+	
+			this.colorize();
+			return this.run(status);
+		}
+
+		return;
+	}
+	colorize() {
+		const color = this.statuses[$(this.e).attr('class').split('-')[1]];
+		$('.container-iksrDt').css('background', color);
+	}
+	revert() {
+		$('.container-iksrDt').css('background', 'rgba(32,34,37,.3)');
 	}
 	stop() {
-		clearInterval(this.interval);
+		this.o = 0;
+		this.revert();
 		this.log('Stopped');
-		this.color = null;
-		this.statuses = null;
-		this.interval = null;
-		this.id = null;
 	}
 	load() {
 		this.log('Loaded');
 	}
 	log(text) {
+		if(typeof text !== 'string') return;
 		return console.log(`%c[${this.getName()}]%c ${text}`, 'color: hsl(12, 100%, 50%);', '');
 	}
 	getName() {
@@ -50,7 +65,4 @@ class statusProfileContainer {
 	getDescription() {
 		return 'Changes the profile area container to be the same colour as your status!';
 	}
-	getSettingsPanel() {
-		return;
-	}
-}
+};
