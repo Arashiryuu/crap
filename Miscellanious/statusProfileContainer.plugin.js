355c49a32da5b061d8ddbd17492a69bda972e9f8
Arashiryuu <walkerdd01@googlemail.com>
2018-04-02 18:15:00 +0100

    Update statusProfileContainer.plugin.js

--- Miscellanious/statusProfileContainer.plugin.js
+++ Miscellanious/statusProfileContainer.plugin.js
@@ -2,24 +2,47 @@
 
 class statusProfileContainer {
 	constructor() {
+		this.initialized = false;
 		this.o = 1;
-		this.id = '238108500109033472';
-		this.e = `.avatar-small[style*="${this.id}"] .status`;
+		this.id = '';
+		this.e = '';
 		this.statuses = {
 			'online': 'rgb(67, 181, 129)',
 			'idle': 'rgb(250, 166, 26)',
 			'dnd': 'rgb(240, 71, 71)',
+			'offline': 'rgb(116, 127, 141)',
 			'invisible': 'rgb(116, 127, 141)',
 			'streaming': 'rgb(89, 54, 149)'
 		};
 	}
+
 	start() {
 		this.log('Starting');
-		this.o = 1;
+		let libraryScript = document.getElementById('zeresLibraryScript');
+		if(!libraryScript) {
+			libraryScript = document.createElement('script');
+			libraryScript.id = 'zeresLibraryScript';
+			libraryScript.src = 'https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js';
+			libraryScript.type = 'text/javascript';
+			document.head.appendChild(libraryScript);
+		}
+
+		if(typeof window.ZeresLibrary !== 'undefined') this.initialize();
+		else libraryScript.addEventListener('load', () => this.initialize());
+	}
+
+	initialize() {
+		this.id = PluginUtilities.getCurrentUser().id;
+		this.e = `.avatar-small[style*="${this.id}"] .status`;
 		const status = $(this.e).attr('class');
+		this.o = 1;
+		
+		this.initialized = true;
+		
 		this.colorize();
 		this.run(status);
 	}
+
 	run(o) {
 		if(this.o) {
 			const status = $(this.e).attr('class');
@@ -34,34 +57,42 @@ class statusProfileContainer {
 
 		return;
 	}
+
 	colorize() {
 		const color = this.statuses[$(this.e).attr('class').split('-')[1]];
 		$('.container-iksrDt').css('background', color);
 	}
+
 	revert() {
-		$('.container-iksrDt').css('background', 'rgba(32,34,37,.3)');
+		$('.container-iksrDt').attr('style', '');
 	}
+
 	stop() {
 		this.o = 0;
 		this.revert();
 		this.log('Stopped');
 	}
+
 	load() {
 		this.log('Loaded');
 	}
-	log(text) {
-		if(typeof text !== 'string') return;
-		return console.log(`%c[${this.getName()}]%c ${text}`, 'color: hsl(12, 100%, 50%);', '');
+
+	log(...t) {
+		return console.log(`%c[${this.getName()}]%c`, 'color: hsl(12, 100%, 50%);', '', ...t);
 	}
+
 	getName() {
 		return 'statusProfileContainer';
 	}
+	
 	getAuthor() {
 		return 'Arashiryuu';
 	}
+
 	getVersion() {
-		return '1';
+		return '1.0.0';
 	}
+
 	getDescription() {
 		return 'Changes the profile area container to be the same colour as your status!';
 	}
