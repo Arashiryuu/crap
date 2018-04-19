be20baaa90032084feaf2d736eb806557269b9cb
Arashiryuu <walkerdd01@googlemail.com>
2018-04-19 12:30:59 +0100

    chatUserIDs v1.1.0

--- chatUserIDs.plugin.js
+++ chatUserIDs.plugin.js
@@ -26,7 +26,14 @@
 
 class chatUserIDs {
 	constructor() {
-		this.css = `<style id="chatUserIDsCSS" type="text/css">
+		this.initialized = false;
+		this.default = {
+			compact: false
+		};
+		this.settings = this.default;
+		this.guild;
+		this.member;
+		this.css = `<style id="ChatIDsCSS" type="text/css">
 			@import 'https://fonts.googleapis.com/css?family=Roboto|Inconsolata';
 
 			#tagID {
@@ -54,6 +61,18 @@ class chatUserIDs {
 			.message-group .comment .message.first ~ div > .edit-message .edit-container-inner .old-h2 {
 				display: none;
 			}
+
+			.message-group.compact .markup .timestamp {
+				order: 0;
+			}
+
+			.message-group.compact .markup .username-wrapper {
+				order: 1;
+			}
+
+			.message-group.compact .markup .message-content {
+				order: 2;
+			}
 		</style>`;
 		
 		this.editObs = new MutationObserver((changes) => {
@@ -76,10 +95,32 @@ class chatUserIDs {
 	}
 
 	start() {
+		this.log('Started');
+		let libraryScript = document.getElementById('zeresLibraryScript');
+		if(!libraryScript) {
+			libraryScript = document.createElement('script');
+			libraryScript.id = 'zeresLibraryScript';
+			libraryScript.src = 'https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js';
+			libraryScript.type = 'text/javascript';
+			document.head.appendChild(libraryScript);
+		}
+
+		if(typeof window.ZeresLibrary !== 'undefined') this.initialize();
+		else libraryScript.addEventListener('load', () => this.initialize());
+	}
+
+	initialize() {
+		PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), this.downLink);
+		PluginUtilities.loadSettings(this.getName(), this.default);
+
 		$('head').append(this.css);
 		this.chatObserve();
 		this.attachID();
-		this.log('Started');
+		this.guild = InternalUtilities.WebpackModules.findByUniqueProperties(['getLastSelectedGuildId']).getGuildId;
+		this.member = InternalUtilities.WebpackModules.findByUniqueProperties(['getMember']).getMember;
+		this.initialized = true;
+
+		PluginUtilities.showToast(`${this.getName()} ${this.getVersion()} has started.`, { type: 'info', icon: true, timeout: 2e3 });
 	}
 
 	chatObserve() {
@@ -93,7 +134,7 @@ class chatUserIDs {
 	}
 
 	observer({ addedNodes, removedNodes }) {
-		if(addedNodes.length && addedNodes[0].classList && (addedNodes[0].classList.contains('messages-wrapper') || addedNodes[0].classList.contains('chat'))) {
+		if(addedNodes.length && addedNodes[0].classList && ['app', 'chat', 'messages-wrapper'].some((u) => addedNodes[0].classList.contains(u))) {
 			this.chatDiscon();
 			this.attachID();
 			this.chatObserve();
@@ -112,30 +153,71 @@ class chatUserIDs {
 	attachID() {
 		if(!document.querySelector('.message-group')) return;
 		try {
-			$('.message-group.hide-overflow').each((index, post) => {
-				if(post.nodeType === 1 && post instanceof Element) {
-					if($(post).find('.tagID') && $(post).find('.tagID').length > 0) return;
-					if(!(post instanceof Element)) return;
-					const elem = `<span id="tagID" class="tagID">${this.getReactInstance(post).return.memoizedProps.messages[0].author.id}</span>`;
-					$(post).find('.username-wrapper').before(elem);
-					$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
-				}
-			});
-			if($('.message-group:not(.hide-overflow)').length > 0) {
-				$('.message-group:not(.hide-overflow)').each((index, post) => {
-					if($(post).find('.tagID') && $(post).find('.tagID').length > 0) return;
-					if(!(post instanceof Element)) return;
-					if(this.getReactInstance(post).return.key && this.getReactInstance(post).return.key.includes('upload')) {
-						const elem = `<span id="tagID" class="tagID">${this.getReactInstance(post).return.memoizedProps.user.id}</span>`;
-						$(post).find('.username-wrapper').before(elem);
-						$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
-					}
-					else if(this.getReactInstance(post).return.memoizedProps.messages && this.getReactInstance(post).return.memoizedProps.messages.length > 0) {
-						const elem = `<span id="tagID" class="tagID">${this.getReactInstance(post).return.memoizedProps.messages[0].author.id}</span>`;
+			if(!this.settings.compact) {
+				$('.message-group.hide-overflow').each((index, post) => {
+					if(post.nodeType === 1 && post instanceof Element) {
+						if($(post).find('.tagID') && $(post).find('.tagID').length > 0) return;
+						if(!(post instanceof Element)) return;
+						const author = this.getReactInstance(post).return.memoizedProps.messages[0].author;
+						const elem = `<span id="tagID" class="tagID">${author.id}</span>`;
 						$(post).find('.username-wrapper').before(elem);
 						$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
 					}
 				});
+				if($('.message-group:not(.hide-overflow)').length > 0) {
+					$('.message-group:not(.hide-overflow)').each((index, post) => {
+						if($(post).find('.tagID') && $(post).find('.tagID').length > 0) return;
+						if(!(post instanceof Element)) return;
+						if(this.getReactInstance(post).return.key && this.getReactInstance(post).return.key.includes('upload')) {
+							const author = this.getReactInstance(post).return.memoizedProps.user;
+							const elem = `<span id="tagID" class="tagID">${author.id}</span>`;
+							$(post).find('.username-wrapper').before(elem);
+							$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
+						}
+						else if(this.getReactInstance(post).return.memoizedProps.messages && this.getReactInstance(post).return.memoizedProps.messages.length > 0) {
+							const author = this.getReactInstance(post).return.memoizedProps.messages[0].author;
+							const elem = `<span id="tagID" class="tagID">${author.id}</span>`;
+							$(post).find('.username-wrapper').before(elem);
+							$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
+							}
+						}
+					});
+				}
+			} else {
+				$('.message-group.hide-overflow.compact').each((index, post) => {
+					if(post.nodeType === 1 && post instanceof Element) {
+						if($(post).find('.tagID') && $(post).find('.tagID').length > 0) return;
+						if(!(post instanceof Element)) return;
+						const author = this.getReactInstance(post).return.memoizedProps.messages[0].author;
+						const elem = `<span id="tagID" class="tagID">${author.id}</span>\u200b`;
+						$(post).find('.username-wrapper .user-name').before(elem);
+						$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
+					}
+				});
+				if($('.message-group:not(.hide-overflow).compact').length > 0) {
+					$('.message-group:not(.hide-overflow).compact').each((index, post) => {
+						if($(post).find('.tagID') && $(post).find('.tagID').length > 0) return;
+						if(!(post instanceof Element)) return;
+						if(this.getReactInstance(post).return.key && this.getReactInstance(post).return.key.includes('upload')) {
+							const author = this.getReactInstance(post).return.memoizedProps.user;
+							const elem = `<span id="tagID" class="tagID">${author.id}</span>\u200b`;
+							$(post).find('.username-wrapper .user-name').before(elem);
+							$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
+							if(this.settings.usernames) {
+								const chatName = $(post).find('.username-wrapper .user-name');
+								if(chatName && chatName.text() !== author.username) {
+									chatName.text(author.username);
+								}
+							}
+						}
+						else if(this.getReactInstance(post).return.memoizedProps.messages && this.getReactInstance(post).return.memoizedProps.messages.length > 0) {
+							const author = this.getReactInstance(post).return.memoizedProps.messages[0].author;
+							const elem = `<span id="tagID" class="tagID">${author.id}</span>\u200b`;
+							$(post).find('.username-wrapper .user-name').before(elem);
+							$(post).find('.tagID').off('dblclick.chatID').on('dblclick.chatID', (e) => this.dblClickID(e));
+						}
+					});
+				}
 			}
 		}
 		catch(e) {
@@ -172,6 +254,29 @@ class chatUserIDs {
 		return console.error(`[%c${this.getName()}%c]`, 'color: #59F;', '', ...error);
 	}
 
+	genSettings(panel) {
+		new PluginSettings.ControlGroup('Plugin Settings', () => PluginUtilities.saveSettings(this.getName(), this.settings)).appendTo(panel).append(
+			new PluginSettings.PillButton('Display Mode', 'The user\'s display mode.', 'Cozy', 'Compact', this.settings.compact, (i) => this.settings.compact = i)
+		);
+
+		const resetButton = $('<button>', {
+			type: 'button',
+			text: 'Reset To Default',
+			style: 'float: right;'
+		}).on('click.reset', () => {
+			this.settings = this.default;
+			PluginUtilities.saveSettings(this.getName(), this.settings);
+			panel.empty();
+			this.genSettingsPanel(panel);
+		});
+
+		panel.append(resetButton);
+	}
+
+	get downLink() {
+		return `https://raw.githubusercontent.com/Arashiryuu/crap/master/ToastIntegrated/${this.getName()}/${this.getName()}.plugin.js`;
+	}
+
 	getName() {
 		return 'chatUserIDs';
 	}
@@ -181,11 +286,17 @@ class chatUserIDs {
 	}
 
 	getVersion() {
-		return '1.0.4';
+		return '1.1.0';
 	}
 
 	getDescription() {
 		return 'Adds a user\'s ID next to their name in chat, makes accessing a user ID simpler. Double-click to copy the ID.';
+	}
+
+	getSettingsPanel() {
+		const panel = $('<form>').addClass('form').css('width', '100%');
+		if(this.initialized) this.genSettings(panel);
+		return panel[0];
 	}
 };
 
