1dc118ff111f7e7bcd735ccc1510c89c697c62f5
Arashiryuu <walkerdd01@googlemail.com>
2018-09-06 00:03:18 +0100

    Update jsMaterialThemeCodeblocks.plugin.js

--- Miscellanious/jsMaterialThemeCodeblocks.plugin.js
+++ Miscellanious/jsMaterialThemeCodeblocks.plugin.js
@@ -1,172 +1,268 @@
-//META{"name":"JSMaterialThemeCodeblocks"}*//
+//META{"name":"JSMaterialThemeCodeblocksRedux","displayName":"JSMaterialThemeCodeblocksRedux","website":"https://github.com/Arashiryuu","source":"https://github.com/Arashiryuu/crap"}*//
 
 /*@cc_on
 @if (@_jscript)
 	
 	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+	var shell = WScript.CreateObject('WScript.Shell');
+	var fs = new ActiveXObject('Scripting.FileSystemObject');
+	var pathPlugins = shell.ExpandEnvironmentStrings('%APPDATA%\\BetterDiscord\\plugins');
 	var pathSelf = WScript.ScriptFullName;
 	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	shell.Popup('It looks like you\'ve mistakenly tried to run me directly. \n(Don\'t do that!)', 0, 'I\'m a plugin for BetterDiscord', 0x30);
 	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
+		shell.Popup('I\'m in the correct folder already.\nJust reload Discord with Ctrl+R.', 0, 'I\'m already installed', 0x40);
 	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		shell.Popup('I can\'t find the BetterDiscord plugins folder.\nAre you sure it\'s even installed?', 0, 'Can\'t install myself', 0x10);
+	} else if (shell.Popup('Should I copy myself to BetterDiscord\'s plugins folder for you?', 0, 'Do you need some help?', 0x34) === 6) {
 		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
 		// Show the user where to put plugins in the future
-		shell.Exec("explorer " + pathPlugins);
-		shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
+		shell.Exec('explorer ' + pathPlugins);
+		shell.Popup('I\'m installed!\nJust reload Discord with Ctrl+R.', 0, 'Successfully installed', 0x40);
 	}
 	WScript.Quit();
 
 @else@*/
 
-class JSMaterialThemeCodeblocks {
-	constructor() {
-		this.messageList = ['container-1YxwTf', 'message-1PNnaP', 'markup-2BOw-j'];
-		this.switchList = ['app', 'chat-3bRxxu', 'messagesWrapper-3lZDfY'];
+var JSMaterialThemeCodeblocksRedux = (() => {
 
-		this.selectors = [
-			'.hljs[class~="js" i] .hljs-keyword', 
-			'.hljs[class~="jsx" i] .hljs-keyword', 
-			'.hljs[class~="javascript" i] .hljs-keyword',
-			'.hljs[class~="js" i] .hljs-built_in',
-			'.hljs[class~="jsx" i] .hljs-built_in',
-			'.hljs[class~="javascript" i] .hljs-built_in'
-		];
+	/* Setup */
 
-		this.keywords = {
-			'instanceof':'instanceof',
-			'arguments':'arguments',
-			'parseInt':'parseInt',
-			'require':'require',
-			'typeof':'typeof',
-			'isNaN':'isNaN',
-			'this':'this',
-			'void':'void',
-			'new':'new',
-			'in':'in',
-			'of':'of'
-		};
+	const config = {
+		main: 'index.js',
+		info: {
+			name: 'JSMaterialThemeCodeblocksRedux',
+			authors: [
+				{
+					name: 'Arashiryuu',
+					discord_id: '238108500109033472',
+					github_username: 'Arashiryuu',
+					twitter_username: ''
+				}
+			],
+			version: '1.0.0',
+			description: 'Applies the "Material Theme" to JavaScript codeblocks.',
+			github: 'https://github.com/Arashiryuu',
+			github_raw: ''
+		}
 	};
 
-	createThisClass() {
-		try {
-			let kws, t, w;
-			for (const s of this.selectors) {
-				kws = document.querySelectorAll(s);
-				for (const k of kws) {
-					t = k.innerText;
-					w = this.getWord(t);
-					if (w && !$(k).hasClass(w)) {
-						$(k).addClass(w);
+	/* Build */
+
+	const buildPlugin = ([Plugin, Api]) => {
+		const { Toasts, Logger, DOMTools, DiscordModules, WebpackModules, DiscordSelectors } = Api;
+		
+		return class JSMaterialThemeCodeblocksRedux extends Plugin {
+			constructor() {
+				super();
+				this.switchList = [
+					'app',
+					DiscordSelectors.TitleWrap.chat.value.slice(2),
+					WebpackModules.getByProps('messages', 'messagesWrapper').messagesWrapper
+				];
+				this.messageList = [
+					DiscordSelectors.Messages.container.value.slice(2),
+					DiscordSelectors.Messages.message.value.slice(2)
+				];
+				this.selectors = [
+					'.hljs[class~="js" i] .hljs-keyword', 
+					'.hljs[class~="jsx" i] .hljs-keyword', 
+					'.hljs[class~="javascript" i] .hljs-keyword',
+					'.hljs[class~="js" i] .hljs-built_in',
+					'.hljs[class~="jsx" i] .hljs-built_in',
+					'.hljs[class~="javascript" i] .hljs-built_in'
+				];
+				this.keywords = {
+					'instanceof':'instanceof',
+					'arguments':'arguments',
+					'parseInt':'parseInt',
+					'require':'require',
+					'typeof':'typeof',
+					'isNaN':'isNaN',
+					'this':'this',
+					'void':'void',
+					'new':'new',
+					'in':'in',
+					'of':'of'
+				};
+				this.stylesheet;
+			}
+
+			/* Methods */
+
+			onStart() {
+				Toasts.info(`${this.name} ${this.version} has started!`, { icon: true, timeout: 2e3 });
+				this.injectCSS();
+				this.addClasses();
+			}
+
+			addClasses() {
+				for (const selector of this.selectors) {
+					const selection = DOMTools.queryAll(selector);
+					for (const selected of selection) {
+						const { innerText: text } = selected;
+						const className = this.getClass(text);
+						if (className && !DOMTools.hasClass(selected, className)) DOMTools.addClass(selected, className);
 					}
 				}
 			}
-		} catch(e) {
-			this.err(e);
-		}
-	};
 
-	paramParse() {
-		let params = document.querySelectorAll('.hljs-params:not(.hljs-no-param)');
+			getClass(name) {
+				if (this.keywords.hasOwnProperty(name)) return this.keywords[name];
+				return null;
+			}
 
-		if (!params.length) return;
-		
-		const noParam = [...params].filter((param) => param.textContent === '()');
+			paramParse() {
+				let params = DOMTools.queryAll('.hljs-params:not(.hljs-no-param)');
+				if (!params.length) return;
+				const noParam = Array.from(params).filter((param) => param.textContent === '()');
+				if (noParam.length) {
+					for (const param of noParam) DOMTools.addClass(param, 'hljs-no-param');
+				}
+				Logger.log(params);
+				params = DOMTools.queryAll('.hljs-params:not(.hljs-no-param)');
+				Logger.log(params);
+				if (!params.length) return;
+				for (const param of params) {
+					if (param.innerHTML.indexOf(',') > -1) {
+						const split = param.innerHTML.split(',');
+						const len = split.length;
+						for (let i = 0; i < len; i++) split[i] = split[i].trim();
+						param.innerHTML = split.join('<span class="hljs-separator">, </span>');
+					}
+				}
+			}
 
-		if (noParam.length) {
-			for (const param of noParam) param.classList.add('hljs-no-param');
-		}
+			injectCSS() {
+				if (this.stylesheet || document.contains(this.stylesheet)) return;
+				this.stylesheet = DOMTools.parseHTML(`<link id="${this.short}" rel="preload stylesheet" as="style" href="${this.cdn}"/>`);
+				DOMTools.appendTo(this.stylesheet, document.head);
+			}
 
-		params = document.querySelectorAll('.hljs-params:not(.hljs-no-param)');
+			removeCSS() {
+				if (this.stylesheet && document.contains(this.stylesheet)) this.stylesheet.remove();
+			}
 
-		if (!params.length) return;
+			onStop() {
+				this.removeCSS();
+				Toasts.info(`${this.name} ${this.version} has stopped!`, { icon: true, timeout: 2e3 });
+			}
 
-		for (const param of params) {
-			if (param.innerHTML.indexOf(',') > -1) {
-				const split = param.innerHTML.split(',')
-					, len = split.length;
-				for (let i = 0; i < len; i++) split[i] = split[i].trim();
-				param.innerHTML = split.join('<span class="hljs-separator">, </span>');
+			/* Observer */
+
+			observer({ addedNodes }) {
+				if (addedNodes.length && addedNodes[0].classList && this.switchList.includes(addedNodes[0].classList[0])) {
+					setTimeout(() => { this.addClasses(); setTimeout(() => this.paramParse(), 500); }, 500);
+				} else if (addedNodes.length && addedNodes[0].classList && this.messageList.includes(addedNodes[0].classList[addedNodes[0].classList.length - 1])) {
+					setTimeout(() => { this.addClasses(); setTimeout(() => this.paramParse(), 500); }, 500);
+				}
+			}
+
+			/* Getters */
+
+			get [Symbol.toStringTag]() {
+				return 'Plugin';
+			}
+
+			get cdn() {
+				return 'https://gitcdn.xyz/repo/Arashiryuu/crap/master/Miscellanious/jsMaterialThemeCodeblocksCSS/src.css';
+			}
+
+			get name() {
+				return config.info.name;
+			}
+
+			get short() {
+				let string = '';
+
+				for (let i = 0, len = config.info.name.length; i < len; i++) {
+					const char = config.info.name[i];
+					if (char === char.toUpperCase()) string += char;
+				}
+
+				return string;
+			}
+
+			get author() {
+				return config.info.authors.map((author) => author.name).join(', ');
+			}
+
+			get version() {
+				return config.info.version;
+			}
+
+			get description() {
+				return config.info.description;
 			}
 		}
 	};
 
-	getWord(text) {
-		if (this.keywords.hasOwnProperty(text)) {
-			return this.keywords[text];
+	/* Finalize */
+
+	return !global.ZeresPluginLibrary 
+		? class {
+			getName() {
+				return this.name.replace(/\s+/g, '');
+			}
+
+			getAuthor() {
+				return this.author;
+			}
+
+			getVersion() {
+				return this.version;
+			}
+
+			getDescription() {
+				return this.description;
+			}
+
+			stop() {
+				Logger.log('Stopped!');
+			}
+
+			load() {
+				window.BdApi.alert('Missing Library', `The library plugin needed for ${config.info.name} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js">Click here to download the library!</a>`);
+			}
+
+			start() {
+				Logger.log('Started!');
+			}
+
+			/* Getters */
+
+			get [Symbol.toStringTag]() {
+				return 'Plugin';
+			}
+
+			get name() {
+				return config.info.name;
+			}
+
+			get short() {
+				let string = '';
+
+				for (let i = 0, len = config.info.name.length; i < len; i++) {
+					const char = config.info.name[i];
+					if (char === char.toUpperCase()) string += char;
+				}
+
+				return string;
+			}
+
+			get author() {
+				return config.info.authors.map((author) => author.name).join(', ');
+			}
+
+			get version() {
+				return config.info.version;
+			}
+
+			get description() {
+				return config.info.description;
+			}
 		}
-
-		return null;
-	};
-
-	log() {
-		return console.log('%c[JSMaterialThemeCodeblocks]', 'color: #F95479; text-shadow: 0 0 1px black, 0 0 2px black, 0 0 3px black;', ...arguments);
-	};
-
-	err() {
-		return console.error('%c[JSMaterialThemeCodeblocks]', 'color: #F95479; text-shadow: 0 0 1px black, 0 0 2px black, 0 0 3px black;', ...arguments);
-	};
-
-	inject() {
-		let sheet = document.getElementById('MaterialCodeblocksCSS');
-		if (!sheet || !document.contains(sheet)) {
-			sheet = document.createElement('link');
-			sheet.setAttribute('id', 'MaterialCodeblocksCSS');
-			sheet.setAttribute('rel', 'stylesheet preload');
-			sheet.setAttribute('as', 'style');
-			sheet.setAttribute('href', 'https://gitcdn.xyz/repo/Arashiryuu/crap/master/Miscellanious/jsMaterialThemeCodeblocksCSS/src.css');
-			document.head.appendChild(sheet);
-		}
-	}
-
-	eject() {
-		const sheet = document.getElementById('MaterialCodeblocksCSS');
-		if (sheet && document.contains(sheet)) sheet.remove();
-	}
-
-	start() {
-		this.inject();
-		this.createThisClass();
-		this.log('Started.');
-	};
-
-	stop() {
-		this.eject();
-		this.log('Stopped.');
-	};
-
-	load() {
-		this.log('Loaded.');
-	};
-
-	observer({ addedNodes }) {
-		if (addedNodes.length && addedNodes[0].classList && this.messageList.includes(addedNodes[0].classList[addedNodes[0].classList.length - 1])) {
-			setTimeout(() => { this.createThisClass(); setTimeout(() => this.paramParse(), 500); }, 250);
-		} else if (addedNodes.length && addedNodes[0].classList && this.switchList.includes(addedNodes[0].classList[0])) {
-			setTimeout(() => { this.createThisClass(); setTimeout(() => this.paramParse(), 500); }, 250); 
-		}
-	};
-
-	getName() {
-		return 'JSMaterialThemeCodeblocks';
-	};
-
-	getAuthor() {
-		return 'Arashiryuu';
-	};
-
-	getVersion() {
-		return '3.0.0';
-	};
-
-	getDescription() {
-		return 'Applies the "Material Theme" to JavaScript codeblocks.';
-	};
-};
+		: buildPlugin(global.ZeresPluginLibrary.buildPlugin(config));
+})();
 
 /*@end@*/
